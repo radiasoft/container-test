001e007b70555094c3a6d2b7fd560173d96d4306
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2026-02-17 01:29:54 +0000

    Fix #3 test for GITHUB_TOKEN in git

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -3,18 +3,18 @@ build_fedora_base_image
 build_is_public=1
 
 build_as_root() {
-    install_yum_install screen
+    install_yum_install python3
 }
 
 build_as_run_user() {
     cd "$build_guest_conf"
     # Must not match exactly so can check for it in test.sh and
     # this file is included in the layer.
-    if [[ ! $GITHUB_TOKEN =~ some-big-secret ]]; then
-        echo 'GITHUB_TOKEN was not included' 1>&2
-        exit 1
+    if [[ ! ${GITHUB_TOKEN:-} =~ some-big-secret ]]; then
+        build_err 'GITHUB_TOKEN was not included'
     fi
-    local test=~/bin/radia-run-testimage
+    declare test=~/bin/radia-run-testimage
+    # public repo so the credentials don't matter but will be tested
     install -m 755 radia-run-testimage.sh "$test"
     cat >> "$test" <<EOF
 if [[ -e $build_guest_conf ]]; then
@@ -22,4 +22,8 @@ if [[ -e $build_guest_conf ]]; then
     exit 1
 fi
 EOF
+    install_git_clone containers
+    if [[ $(cd containers && git config --get remote.origin.url) =~ some-big-secret ]]; then
+        build_err 'secret is in git remote.origin.url'
+    fi
 }
